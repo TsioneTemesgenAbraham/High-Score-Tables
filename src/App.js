4ca95a2f4306368a6ec61c2e515594988be76edc
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2022-11-18 14:05:35 +0000

    Done until levels 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from "react";
 import "./App.css";
+import allCountryScores from "./score";
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>High Score Per Country</h1>
+      <MakeList />
+    </div>
+  );
+}
+
+function PLayerScore(props) {
+  return (
+    <div>
+      <table className="table">
+        <tbody>
+          <tr>
+            <td>{props.n}</td>
+            <td>{props.s}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function MakeList() {
+  return (
+    <div>
+      {allCountryScores
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((element) => (
+          <>
+            <h3>High Scores: {element.name}</h3>
+            {element.scores
+              .sort((a, b) => b.s - a.s)
+              .map((el) => (
+                <PLayerScore n={el.n} s={el.s} />
+              ))}
+          </>
+        ))}
     </div>
   );
 }
